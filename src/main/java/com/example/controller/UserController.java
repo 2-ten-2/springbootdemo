f8ab480c70d2af8528a9424b0b9d6bc877115a89
2-ten-2 <charontrs1226@gmail.com>
2024-03-24 12:47:07 +0800

    first commit

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("login")
+    public String login () {
+        return "第一个登录信息";
+    }
+
+}
